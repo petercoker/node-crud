304c195d1c2c4132cedcd03b3eeedd9d02654b18
Peter Coker <peter.coker@ellucian.com>
2024-01-03 17:35:12 +0000

    feat(express-post): update the post request

--- index.js
+++ index.js
@@ -1,26 +1,34 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express(); // create express app
+app.use(bodyParser.json()); // parse json
+app.use(bodyParser.urlencoded({ extended: true })); // parse form data encoded in the url 
 const port = 3000; // runs on port 3000
 
-
+let tweets = []
 // / is the base route
-app.get('/', (req, res) => {
-    res.send('GET Hello World!');
+app.get('/tweets', (req, res) => {
+    res.send(JSON.stringify(tweets));
+})
+ 
+app.post('/tweets', (req, res) => {
+    // post tweets
+    const tweetsRequests = req.body.text; // get the text from the body
+    console.log(tweetsRequests) ;
+    tweets.push(tweetsRequests);
+    res.send('POSTED TWEETS: tweets' + tweets);
 })
 
-app.post('/', (req, res) => {
-    res.send('POST Hello World!');
-})
-
-app.put('/', (req, res) => {
+app.put('/tweets', (req, res) => {
     res.send('PUT Hello World!');
 })
 
-app.delete('/', (req, res) => {
+app.delete('/tweets', (req, res) => {
     res.send('DELETE Hello World!');
 })
 
-app.patch('/', (req, res) => {
+app.patch('/tweets', (req, res) => {
     res.send('PATCH Hello World!');
 })
 
