c631ce0b73e16bbecc6a63935e9eeca2189ced6e
Peter Coker <peter.coker@ellucian.com>
2024-01-03 13:33:15 +0000

    feat(file-system): create print hello world from file system

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const http = require('http');
-
+const fileSystem = require('fs');
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World\n');
+    fileSystem.readFile('index.html', (err, data) => {
+        if(err) {
+            res.writeHead(404);
+            res.end('File not found!');
+        } else {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'text/html');
+            res.end(data);
+        }
+    })
 });
 
 server.listen(3000, () => {
