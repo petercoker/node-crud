012b0dbcc684201bf1448cf985e427f4b880d37d
Peter Coker <peter.coker@ellucian.com>
2024-01-03 16:45:26 +0000

    feat(express): used express to create for crud

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
-const http = require('http');
-const greet = require('./greet');
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    // res.end(greet('Peter')); // if the greet module only contains the greet function and nothing else, using module.exports = greet
-    res.end(greet.greet('Peter Coker'));
-});
+const express = require('express');
+const app = express(); // create express app
+const port = 3000; // runs on port 3000
 
-server.listen(3000, () => {
-    console.log('Server running on port 3000');
+
+// / is the base route
+app.get('/', (req, res) => {
+    res.send('GET Hello World!');
 })
 
+app.post('/', (req, res) => {
+    res.send('POST Hello World!');
+})
+
+app.put('/', (req, res) => {
+    res.send('PUT Hello World!');
+})
+
+app.delete('/', (req, res) => {
+    res.send('DELETE Hello World!');
+})
+
+app.patch('/', (req, res) => {
+    res.send('PATCH Hello World!');
+})
+
+// start server
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+})
